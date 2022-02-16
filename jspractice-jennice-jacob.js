23d965c2cf6548388df697d4110b549bbeccf46f
Jennice <jennicelodi@gmail.com>
2022-02-16 14:09:37 -0800

    added js file

---
+++ jspractice-jennice-jacob.js
@@ -0,0 +1,30 @@
+//Challenge 1
+console.log(34 + 71)
+//Challenge 2
+console.log(67 - 123)
+//Challenge 3
+console.log(56 * 23)
+//Challenge 4
+console.log(45 / 5)
+//Challenge 5
+console.log(5 ** 7)
+//Challenge 6
+console.log(33 % 6)
+//Challenge 7
+console.log("jacob".length)
+//Challenge 8
+console.log("jennice".includes("e"))
+//Challenge 9
+console.log("learnacademy".charAt(0))
+//Challenge 10
+console.log("supercalifraglisticexpialidocious".toUpperCase())
+
+//Challenge 11
+console.log( 34 / 3 >= 67 / 2)
+//Challenge 9
+console.log(5 === "5")
+//Challenge 10
+console.log(!3 === 3)
+//Challenge 11
+console.log("LEARN".length === 5 && "Student".length === 10)
+//Challenge 12
