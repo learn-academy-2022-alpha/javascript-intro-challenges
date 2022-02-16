11ec2424ec71eb588d5779da961e332e01a6ce66
Athen Park <athenjpark@gmail.com>
2022-02-16 15:10:35 -0800

    Cleaned up the structure for clarity.

--- intro-athen-ramon.js
+++ intro-athen-ramon.js
@@ -1,15 +1,21 @@
-//console.log(34+71);
-
-//console.log(123-67);
-
-//console.log(56*23);
-//console.log(45/5);
-//console.log(5**7);
-//console.log(33%6);
-//console.log("athen".length);
-//console.log("athen".includes("e"));
-//console.log("ramon".includes("e"));
-//console.log("Hello World"[0]);
-//console.log("hello World".toUpperCase());
-
-
+//Challenge 1
+console.log(34+71);
+//Challenge 2
+console.log(123-67);
+//Challenge 3
+console.log(56*23);
+//Challenge 4
+console.log(45/5);
+//Challenge 5
+console.log(5**7);
+//Challenge 6
+console.log(33%6);
+//Challenge 7
+console.log("athen".length);
+//Challenge 8
+console.log("athen".includes("e"));
+console.log("ramon".includes("e"));
+//Challenge 9
+console.log("Hello World"[0]);
+//Challenge 10
+console.log("hello World".toUpperCase());
