783a982630efd1fdd2437b58760f21beea5ee0ab
Ramon Bolds <ramonbolds98@gmail.com>
2022-02-16 16:09:33 -0600

    javascript intro challenges 1-10

---
+++ intro-athen-ramon.js
@@ -0,0 +1,15 @@
+//console.log(34+71);
+
+//console.log(123-67);
+
+//console.log(56*23);
+//console.log(45/5);
+//console.log(5**7);
+//console.log(33%6);
+//console.log("athen".length);
+//console.log("athen".includes("e"));
+//console.log("ramon".includes("e"));
+//console.log("Hello World"[0]);
+//console.log("hello World".toUpperCase());
+
+
