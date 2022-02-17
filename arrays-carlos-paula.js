bdc5be87742626c93f7aeaaceba11e2a15c22f5e
Paula Nguyen <nguyen.paula5560@gmail.com>
2022-02-17 15:28:36 -0800

    doing some of the challenge

--- arrays-carlos-paula.js
+++ arrays-carlos-paula.js
@@ -22,3 +22,16 @@ console.log(subsetList)
 
 console.log(subsetList.concat('Beans'))
 
+// Write the code that will add the number 0 to the beginning of the array.
+var numbers = [2, 4, 6 , 8, 10]
+console.log(numbers.unshift(0))
+// Write the code that will add the number 12 to the end of the array.
+console.log(numbers.push(12))
+console.log(numbers)
+// Write the code that will remove the first number from the array.
+console.log(numbers.shift())
+console.log(numbers)
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var zero = 0
+var subset = (numbers.concat(zero))
+console.log(subset.reverse())
