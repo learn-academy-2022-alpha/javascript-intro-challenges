c9962b87ec4dcb12f3727cdb4d4113ab42d2825b
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-02-17 11:56:32 -0800

    finished array challenges

--- arrays-alex-dane.js
+++ arrays-alex-dane.js
@@ -12,12 +12,12 @@ console.log(groceryList.concat(groceryList2))
 console.log(groceryList)
 
 // Write the code that will return a subset of the array containing only "chips" and "dip".
-console.log(groceryList.slice(0,2))
+console.log(groceryList.slice(0, 2))
 
 
 // Write the code that will add "beans" to the "chips" and "dip" array.
 
-var slicedarr = groceryList.slice(0,2)
+var slicedarr = groceryList.slice(0, 2)
 console.log(slicedarr.push("beans"))
 console.log(slicedarr)
 
@@ -47,36 +47,81 @@ console.log(numbers)
 
 // Consider the variable:
 
-// var numSet = [2, 13, 6, 8, 4, 2]
+var numSet = [2, 13, 6, 8, 4, 2]
 // Write the code that finds the index of the first appearance of the number 2.
+
+console.log(numSet.indexOf(2))
 
 // Write the code that finds the index of the last appearance of the number 2.
 
+console.log(numSet.lastIndexOf(2))
+
 // Write the code that returns the number at the third index.
+
+console.log(numSet[3])
 
 // Consider the variable:
 
-// var characters = ["y", "a", "r", "r", "a"]
+var characters = ["y", "a", "r", "r", "a"]
 // Write the code that brings all the letters in the characters array together into a string.
+
+console.log(characters.join())
 
 // Write the code that reverses the order of the letters in the characters array and saves it into a variable called charsReversed.
 
+var charsReversed = characters.reverse()
+console.log(charsReversed)
+
 // Write the code that brings all the letters in the charsReversed array together into a string with an asterisk between each letter.
+
+console.log(charsReversed.join("*"))
 
 // Write the code that brings all the letters in the charsReversed array together into a string without separators.
 
+console.log(charsReversed.join(""))
+
 // Create two arrays consisting of three first names of your cohort members in each.
+
+var arr1 = ["Alex", "Dane", "Nate"]
+var arr2 = ["Stevie", "Kendra", "Paula"]
 
 // Write the code that sorts the names in alphabetical order.
 
+var sorted1 = arr1.sort()
+var sorted2 = arr2.sort()
+console.log(sorted1)
+console.log(sorted2)
+
 // Write the code that sorts the names in reverse alphabetical order.
+
+console.log(sorted1.reverse())
+console.log(sorted2.reverse())
 
 // Write the code that sorts all the names in alphabetical order in a single array.
 
+var sortedNames = arr1.concat(arr2).sort()
+console.log(sortedNames)
+
 // Consider the variables:
 
-// var numbers = [42, 221, 71, 7, 18, 87]
-// var oddIndexes = []
+var numbers = [42, 221, 71, 7, 18, 87]
+var oddIndexes = []
 // Write the code that logs the values from the numbers array that are at odd indexes.
 
+for (let i = 0; i < numbers.length; i++) {
+    var current = numbers[i]
+    if (current % 2 !== 0) {
+        console.log(current)
+    }
+}
+
 // Write the code that adds the values from odd indexes into the oddIndexes array.
+
+for (let i = 0; i < numbers.length; i++) {
+    var current = numbers[i]
+    if (current % 2 !== 0) {
+        oddIndexes.push(current)
+    }
+}
+
+console.log(oddIndexes)
