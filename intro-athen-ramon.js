ef1063a4e748dd67f5d2ee78078cc76110ea5067
Athen Park <athenjpark@gmail.com>
2022-02-16 16:54:15 -0800

    JavaScript Conditionals Challenges

---
+++ intro-athen-ramon.js
@@ -0,0 +1,21 @@
+//Challenge 1
+console.log(34+71);
+//Challenge 2
+console.log(123-67);
+//Challenge 3
+console.log(56*23);
+//Challenge 4
+console.log(45/5);
+//Challenge 5
+console.log(5**7);
+//Challenge 6
+console.log(33%6);
+//Challenge 7
+console.log("athen".length);
+//Challenge 8
+console.log("athen".includes("e"));
+console.log("ramon".includes("e"));
+//Challenge 9
+console.log("Hello World"[0]);
+//Challenge 10
+console.log("hello World".toUpperCase());
