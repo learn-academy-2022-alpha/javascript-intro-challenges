9499e3755f5ae7a26c1934d99a9cc9e98129d3d3
Jennice <jennicelodi@gmail.com>
2022-02-17 11:46:09 -0800

    added arrays challenge file

---
+++ arrays-jennice-curtis.js
@@ -0,0 +1,44 @@
+//Write the code that will add "soda" to the end of the original array.
+
+//var groceryList = ["chips", "dip", "cookies"]
+//console.log(groceryList.push("soda"))
+//console.log(groceryList)
+
+//Write the code that will add "granola" to the end of array without altering the original array.
+//var groceryList = ["chips", "dip", "cookies"]
+//var groceryListTwo = ["granola"]
+
+//console.log(groceryList.concat(groceryListTwo))
+//console.log(groceryList)
+
+//Write the code that will return a subset of the array containing only "chips" and "dip".
+//var groceryList = ["chips", "dip", "cookies"]
+//console.log(groceryList.slice(0,2))
+
+//Write the code that will add "beans" to the "chips" and "dip" array.
+//var groceryListTwo = groceryList.slice(0,2)
+//console.log(groceryListTwo.push("Beans"))
+//console.log(groceryListTwo)
+//console.log(groceryList)
+
+//Write the code that will add the number 0 to the beginning of the array.
+var numbers = [2, 4, 6, 8, 10]
+//console.log(numbers.unshift(0))
+//console.log(numbers)
+//Write the code that will add the number 12 to the end of the array.
+
+//console.log(numbers.push(12))
+//console.log(numbers)
+
+//Write the code that will remove the first number from the array.
+
+//console.log(numbers.shift())
+//console.log(numbers)
+
+//Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+
+console.log(numbers.slice(0,5))
+var numbersTwo = numbers.slice(0,5)
+var justZero = [0]
+console.log(justZero.concat(numbersTwo))
+console.log(numbers)
