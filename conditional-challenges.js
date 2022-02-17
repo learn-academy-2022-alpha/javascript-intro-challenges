90c21b2b399ed59ec0273f1e2a3e4fdef0cb845f
Curtis A. Liu <Curtisaliu@gmail.com>
2022-02-17 11:01:42 -0800

    conditional challenges answers

---
+++ conditional-challenges.js
@@ -0,0 +1,42 @@
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+var item="1000"
+if(item <= 100){
+  console.log("in budget")
+} else{
+  console.log("not in budget")
+}
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+var hungry="hungry"
+if(hungry === hungry){
+  console.log("eat food")
+} else{
+  console.log("keep coding")
+}
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var trafficLight="yellow"
+if(trafficLight == "red"){
+  console.log("stop")
+} else if(trafficLight == "green"){
+  console.log("go")
+} else if(trafficLight == "yellow"){
+  console.log("slow down")
+}
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+var pizza=0
+var wings=500
+if(pizza>wings){
+  console.log(pizza)
+} else if(wings>pizza){
+  console.log(wings)
+}else if(pizza == wings){
+  console.log("the numbers are the same")
+}
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+if (pizza == 0){
+  console.log("0")
+} else if(pizza%2 != 0){
+  console.log("odd")
+} else if (pizza%2 == 0){
+  console.log("even")
+}
+console.log(0/0)
