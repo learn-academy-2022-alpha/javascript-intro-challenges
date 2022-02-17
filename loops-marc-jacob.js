554bf204d8fcdd48f54a2d7d648795221b03493c
Valeriano-WebDev <Valeriano-WebDev@gmail.com>
2022-02-17 15:51:35 -0800

    repush, refactored fizzbuzz

--- loops-marc-jacob.js
+++ loops-marc-jacob.js
@@ -77,12 +77,12 @@ for (let i = 0; i < 16; i++ ){
 
 for (var i = 1; i < 101; i++ ) {
     if (i % 15 == 0) {
-        console.log("fiz")
+        console.log("fizbuzz")
     } else if (i % 3 == 0) {
-        console.log("buzz")
+        console.log("fizz")
     } else if (i % 5 == 0) {
         {
-            console.log("fizzbuzz")
+            console.log("buzz")
         }
     } else {
         {
