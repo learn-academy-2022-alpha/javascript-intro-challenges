99917e3ef9271f51b24ed399d384b33a6b99bedf
Curtis A. Liu <Curtisaliu@gmail.com>
2022-02-17 15:03:25 -0800

    arrays challenges answers

--- arrays-jennice-curtis.js
+++ arrays-jennice-curtis.js
@@ -22,7 +22,7 @@
 //console.log(groceryList)
 
 //Write the code that will add the number 0 to the beginning of the array.
-var numbers = [2, 4, 6, 8, 10]
+//var numbers = [2, 4, 6, 8, 10]
 //console.log(numbers.unshift(0))
 //console.log(numbers)
 //Write the code that will add the number 12 to the end of the array.
@@ -37,8 +37,19 @@ var numbers = [2, 4, 6, 8, 10]
 
 //Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
 
-`console.log(numbers.slice(0,5))
-var numbersTwo = numbers.slice(0,5)
-var justZero = [0]
-console.log(justZero.concat(numbersTwo))
-console.log(numbers)
+// `console.log(numbers.slice(0,5))
+// var numbersTwo = numbers.slice(0,5)
+// var justZero = [0]
+// console.log(justZero.concat(numbersTwo))
+// console.log(numbers)
+
+// var numSet = [2, 13, 6, 8, 4, 2]
+// Write the code that finds the index of the first appearance of the number 2.
+// console.log(numSet.indexOf(2))
+
+// Write the code that finds the index of the last appearance of the number 2.
+// console.log(numSet.lastIndexOf(2))
+
+// Write the code that returns the number at the third index.
+var [one, two, three, four, five, six] =  [2, 13, 6, 8, 4, 2]
+console.log(four)
