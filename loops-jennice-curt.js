52e4ebae7de7ae5d98fad5e72b9bac4b9eba5ffd
Curtis A. Liu <Curtisaliu@gmail.com>
2022-02-17 15:21:32 -0800

    loop challenges answers

---
+++ loops-jennice-curt.js
@@ -0,0 +1,19 @@
+// Logging values with for loops
+// Write a for loop that logs each number from 1 - 20.
+// for(let i = 0; i < 21; i++){
+//   console.log(i)
+// }
+
+// Write a for loop that logs the result of each number from 1 - 20 tripled.
+// for(let i = 0; i < 21; i++){
+//   console.log(i * 3)
+// }
+
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
+for(let i = 0; i < 21; i++){
+  if(i % 2 == 0){
+    console.log(i)
+  } else{
+    console.log("ODD")
+  }
+}
