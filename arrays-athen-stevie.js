4e17a5952ea6759f265e3ba82bf1f0ceaa0de424
Athen Park <athenjpark@gmail.com>
2022-02-17 15:08:10 -0800

    Attempted last two problems

--- arrays-athen-stevie.js
+++ arrays-athen-stevie.js
@@ -65,9 +65,11 @@ console.log(alphaCohortTeam.sort())
 var numbers = [42, 221, 71, 7, 18, 87]
 var oddIndexes = []
 // Write the code that logs the values from the numbers array that are at odd indexes.
-if(numbers.indexOf(){
-  console.log(oddIndex)
-} else {
-  console.log("Try again")
+for(let i = 0; i < numbers.length; i++){
+  if(numbers.indexOf(i) % 2 !== 0){
+    console.log(numbers[i])
+  } else {
+    console.log("Try again")
+  }
 }
 // Write the code that adds the values from odd indexes into the oddIndexes array.
