ecc0eac9b809cb1e4eef3a8933a918c7c7bece39
Alex Ranga <alexisranga@gmail.com>
2022-02-17 15:49:13 -0800

    started loops challenges

--- loops-dane-alex.js
+++ loops-dane-alex.js
@@ -13,3 +13,54 @@
 //         console.log("ODD")
 //     }
 // }
+
+
+// Looping over an array. Consider this variable:
+// var nums = [3, 57, -9, 20, 67]
+
+// Create a loop that will log the highest number from the array. Expected output --> 67
+var nums = [3, 57, -9, 20, 67]
+var largestnum
+for (let i = 0; i<nums.length; i++){
+if(!largestnum){largestnum=nums[i]}
+if(nums[i]>largestnum){largestnum=nums[i]}
+}
+console.log(largestnum)
+
+
+// Create a loop that will log the lowest number from the array Expected output --> -9
+
+var nums = [3, 57, -9, 20, 67]
+var lowestnum
+for (let i = 0; i<nums.length; i++){
+if(!lowestnum){lowestnum=nums[i]}
+if(nums[i]<lowestnum){lowestnum=nums[i]}
+}
+console.log(lowestnum)
+
+
+// Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+var nums = [3, 57, -9, 20, 67]
+
+for(let i=0; i<nums.length; i++){
+    console.log(nums[i] % 2)
+}
+
+
+
+// Looping over a string. Consider this variable:
+// var myString = "learn student"
+
+// Write the code that will log the number of times the letter "e" occurs in the string. Expected output --> 2
+
+var myString = "learn student"
+var count = 0
+for(let i=0; i<myString.length; i++){
+    console.log(myString[i])
+    if(myString[i] === "e"){count++}
+}
+console.log(count)
+
+// STRETCH Challenges
+// Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
+// Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
