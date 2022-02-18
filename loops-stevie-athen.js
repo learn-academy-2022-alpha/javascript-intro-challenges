f066eb2cd5d4c4db2e23708be07dcf0ab73c0142
Athen Park <athenjpark@gmail.com>
2022-02-17 17:17:12 -0800

    Cleaned up some code for clarity and readability

--- loops-stevie-athen.js
+++ loops-stevie-athen.js
@@ -27,3 +27,24 @@ for(let i=0; i<1; i++){
     let highestNumber = nums.length - 1
     console.log(orderedNums[highestNumber])
 }
+
+// Create a loop that will log the lowest number from the array
+for(let i=0; i<1; i++){
+  let orderedNums = nums.sort((a,b)=>a-b)
+  let lowestNumber = nums.length - nums.length
+  console.log(orderedNums[lowestNumber])
+}
+// Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+for(let i=0; i< nums.length; i++){
+  console.log(nums[i] % 2)
+}
+// 3. Looping over a string. Consider this variable:
+
+var myString = "learn student"
+
+// Write the code that will log the number of times the letter "e" occurs in the string. Expected output --> 2
+
+// STRETCH Challenges
+// Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
+
+// Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
