02422961f2734e971d7029ce9ec7abc5b60daaa2
Curtis A. Liu <Curtisaliu@gmail.com>
2022-02-18 14:38:05 -0800

    functions challenges answers

--- functions-marc-curtis.js
+++ functions-marc-curtis.js
@@ -32,35 +32,51 @@
 // console.log(multiply(5, 6));
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
-const divisibleBy = (n1, n2) => {
-    if (n1 % n2 === 0) {
-        return `${n1} is evenly divisible by ${5}.`
-    } else {
-        return n1 + " is not evenly divisible by " + n2
-    }
-}
-console.log(divisibleBy(100, 9));
+// const divisibleBy = (n1, n2) => {
+//     if (n1 % n2 === 0) {
+//         return `${n1} is evenly divisible by ${5}.`
+//     } else {
+//         return n1 + " is not evenly divisible by " + n2
+//     }
+// }
+// console.log(divisibleBy(100, 9));
 
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
-const assignGrade = (numberScore) => {
-    if (numberScore < 0) {
-        return "You can't get s score less than 0"
-    } else if (numberScore > 100) {
-        return "You can't get a  score of greater than a 100"
-    }else if (numberScore >= 90) {
-        return "You got an A!"
-    } else if (numberScore >=  80) {
-        return "You got an B!"
-    } else if (numberScore >= 70) {
-        return "You got an C!"
-    } else {
-        return "Soryy, you got an F."
-    }
-}
-console.log(assignGrade(101));
+// const assignGrade = (numberScore) => {
+//     if (numberScore < 0) {
+//         return "You can't get s score less than 0"
+//     } else if (numberScore > 100) {
+//         return "You can't get a  score of greater than a 100"
+//     }else if (numberScore >= 90) {
+//         return "You got an A!"
+//     } else if (numberScore >=  80) {
+//         return "You got an B!"
+//     } else if (numberScore >= 70) {
+//         return "You got an C!"
+//     } else {
+//         return "Soryy, you got an F."
+//     }
+// }
+// console.log(assignGrade(101));
 
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+// const isLonger = (s1, s2) => {
+//   if (s1.length > s2.length){
+//       return s1
+//   } else{
+//     return s2
+//   }
+// }
+// console.log(isLonger("Marc", "Curtis"))
 
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+// const greaterNum = (n1, n2) => {
+//   if (n1 > n2){
+//     return ("n1")
+//   } else {
+//     return ("n2")
+//   }
+// }
+// console.log(greaterNum(5,6))
 
 // Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
