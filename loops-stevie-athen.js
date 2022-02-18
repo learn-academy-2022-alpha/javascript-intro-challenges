42825a549af6f4decfb15c1fe5e6348793f03089
Athen Park <athenjpark@gmail.com>
2022-02-17 16:42:58 -0800

    Loops Challenges

--- loops-stevie-athen.js
+++ loops-stevie-athen.js
@@ -1,11 +1,14 @@
-for(let i=1; i<21; i++){
-    console.log(i)
-}
+// 1. Logging values with for loops
+// Write a for loop that logs each number from 1-20
 
+for(let i=1; i<21; i++){
+  console.log(i)
+}
+// Write a for loop that logs the result of each number from 1-20 tripled.
 for(let i=1; i<21; i++){
     console.log(i * 3)
 }
-
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD"
 for(let i=1; i<21; i++){
     if(i % 2 !== 0){
         console.log("ODD")
@@ -14,14 +17,13 @@ for(let i=1; i<21; i++){
     }
 
 }
+// 2. Looping over an array. Consider this variable:
+// Create a loop that will log the highest number from the array. Expected output --> 67
 
 var nums = [3, 57, -9, 20, 67]
 
-for(let i=0; i<nums.length; i++){
-    if(nums[i]>nums[i++]){
-        console.log(nums[i])
-    }else{
-        console.log(nums[i++])
-    }
-
+for(let i=0; i<1; i++){
+    let orderedNums = nums.sort((a,b)=>a-b)
+    let highestNumber = nums.length - 1
+    console.log(orderedNums[highestNumber])
 }
