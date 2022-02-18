d2a14d12aea3d96393f65e4c142318c49ae1e19c
CarRamos <goodsleep415@gmail.com>
2022-02-18 13:06:45 -0800

    added the function challenge

---
+++ fun-challenge-cr-nb.js
@@ -0,0 +1,64 @@
+// Write a function named marco that returns "polo".
+
+const marco = () => {
+    return 'polo'
+}
+console.log(marco())
+
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+const greeting = (name) => {
+    return `Welocome ${name}!`
+}
+console.log(greeting("Nate"))
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+
+const oddOrEven = (num) => {
+    if ([num]%2 === 0){
+        return "even"
+    } else {
+        return "odd"
+    }
+}
+console.log(oddOrEven(5))
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+const multiplier = (num1) => {
+        return num1 * 3
+}
+console.log(multiplier(5))
+
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+
+const multiply = (numb1, numb2) => {
+    return numb1 * numb2
+}
+console.log(multiply(5,5))
+
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+const divisibleBy = (numbe1, numbe2) => {
+    if ([numbe1] % [numbe2] === 0 ) {
+        return `$[numbe1]
+    }
+  
+    }
+   
+        return numbe1 / numbe2
+  
+}
+console.log(divisibleBy(5,10))
+
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
+
+
+
