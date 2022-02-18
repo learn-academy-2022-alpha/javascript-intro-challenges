d8200c86343fdd27d6822edae1886aff208dd4fe
Paula Nguyen <nguyen.paula5560@gmail.com>
2022-02-18 11:14:08 -0800

    just pushing that back onto there

--- loops-carlos-paula.js
+++ loops-carlos-paula.js
@@ -7,9 +7,17 @@
 // for(let i=1; i<21; i++){
 //   console.log([i]*3)
 // }
-// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
-for(let i=1; i <21; i++){
-  if([i] % 2 !== 0){
-    console.log("ODD")
-  } else {console.log(i)}
+// // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
+// for(let i=1; i <21; i++){
+//   if([i] % 2 !== 0){
+//     console.log("ODD")
+//   } else {console.log(i)}
+// }
+
+var nums = [3, 57, -9, 20, 67]
+
+for(let i = 0; i<nums.length; i++){
+  if(nums[i] % 2 === 1){
+    console.log(nums[i])
+  }
 }
