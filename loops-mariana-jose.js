1e4addc70ea6de9e08e0d5b33967de54380e42c2
josesison1 <learnacademy@LEARNs-Air.lan1>
2022-02-17 16:30:42 -0800

    updated loops js file

--- loops-mariana-jose.js
+++ loops-mariana-jose.js
@@ -1,55 +1,55 @@
 // Challenges
 // Logging values with for loops
 // Write a for loop that logs each number from 1 - 20.
-// for(let i=1; i<=20; i++) {
-//     console.log(i
-// }
+for(let i=1; i<=20; i++) {
+    console.log(i
+}
 // Write a for loop that logs the result of each number from 1 - 20 tripled
-// for(let i=1; i<21; i++){
-//     console.log(i)
-//     console.log(i)
-//     console.log(i)
-// }
+for(let i=1; i<21; i++){
+    console.log(i)
+    console.log(i)
+    console.log(i)
+}
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
-// for(let i=1; i<21; i++){
-//     if (i%2 === 0) {
-//         console.log (i)
-//     } else 
-//     { 
-//         console.log ("odds")
-//     }
-// }
+for(let i=1; i<21; i++){
+    if (i%2 === 0) {
+        console.log (i)
+    } else 
+    { 
+        console.log ("odds")
+    }
+}
 // Looping over an array. Consider this variable:
 var nums = [3, 57, -9, 20, 67]
 // Create a loop that will log the highest number from the array. Expected output --> 67
-// let highestNum;
+let highestNum;
 
-// for (let i=0; i < nums.length; i++){
-//    if (!highestNum) {
-//        highestNum = nums[i]
-//    } if (nums[i] > highestNum) {
-//        highestNum = nums[i]
-//    }
-// }
-//    console.log(highestNum);
+for (let i=0; i < nums.length; i++){
+   if (!highestNum) {
+       highestNum = nums[i]
+   } if (nums[i] > highestNum) {
+       highestNum = nums[i]
+   }
+}
+   console.log(highestNum);
     
 
 // Create a loop that will log the lowest number from the array Expected output --> -9
-// let lowestNum;
+let lowestNum;
 
-// for (let i=0; i < nums.length; i++){
-//    if (!lowestNum) {
-//     lowestNum = nums[i]
-//    } if (nums[i] < lowestNum) {
-//     lowestNum = nums[i]
-//    }
-// }
-//    console.log(lowestNum);
+for (let i=0; i < nums.length; i++){
+   if (!lowestNum) {
+    lowestNum = nums[i]
+   } if (nums[i] < lowestNum) {
+    lowestNum = nums[i]
+   }
+}
+   console.log(lowestNum);
 
 // Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
-// for (let i=0; i < nums.length; i++) {
-//        console.log(nums[i] % 2)
-//    } 
+for (let i=0; i < nums.length; i++) {
+       console.log(nums[i] % 2)
+   } 
 
 // Looping over a string. Consider this variable:
 var myString = "learn student"
