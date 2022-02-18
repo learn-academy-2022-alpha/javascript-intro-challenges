10fc427f1990ba8267251b322f83577833cbd489
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-02-17 16:09:30 -0800

    finished loop challenges

--- loops-dane-alex.js
+++ loops-dane-alex.js
@@ -1,10 +1,16 @@
+// Write a for loop that logs each number from 1 - 20.
+
 // for (let i = 1; i < 21; i++) {
 //     console.log(i)
 // }
 
+// Write a for loop that logs the result of each number from 1 - 20 tripled.
+
 // for (let i = 1; i < 21; i++) {
 //     console.log(i * 3)
 // }
+
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
 
 // for (let i = 1; i < 21; i++) {
 //     if (i % 2 === 0) {
@@ -21,9 +27,9 @@
 // Create a loop that will log the highest number from the array. Expected output --> 67
 var nums = [3, 57, -9, 20, 67]
 var largestnum
-for (let i = 0; i<nums.length; i++){
-if(!largestnum){largestnum=nums[i]}
-if(nums[i]>largestnum){largestnum=nums[i]}
+for (let i = 0; i < nums.length; i++) {
+    if (!largestnum) { largestnum = nums[i] }
+    if (nums[i] > largestnum) { largestnum = nums[i] }
 }
 console.log(largestnum)
 
@@ -32,9 +38,9 @@ console.log(largestnum)
 
 var nums = [3, 57, -9, 20, 67]
 var lowestnum
-for (let i = 0; i<nums.length; i++){
-if(!lowestnum){lowestnum=nums[i]}
-if(nums[i]<lowestnum){lowestnum=nums[i]}
+for (let i = 0; i < nums.length; i++) {
+    if (!lowestnum) { lowestnum = nums[i] }
+    if (nums[i] < lowestnum) { lowestnum = nums[i] }
 }
 console.log(lowestnum)
 
@@ -42,7 +48,7 @@ console.log(lowestnum)
 // Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
 var nums = [3, 57, -9, 20, 67]
 
-for(let i=0; i<nums.length; i++){
+for (let i = 0; i < nums.length; i++) {
     console.log(nums[i] % 2)
 }
 
@@ -55,12 +61,32 @@ for(let i=0; i<nums.length; i++){
 
 var myString = "learn student"
 var count = 0
-for(let i=0; i<myString.length; i++){
+for (let i = 0; i < myString.length; i++) {
     console.log(myString[i])
-    if(myString[i] === "e"){count++}
+    if (myString[i] === "e") { count++ }
 }
 console.log(count)
 
 // STRETCH Challenges
 // Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
+
+for (let i = 0; i < 16; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is even`)
+    } else {
+        console.log(`${i} is odd`)
+    }
+}
 // Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
+
+for (let i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuzz")
+    } else if (i % 3 === 0) {
+        console.log("fizz")
+    } else if (i % 5 === 0) {
+        console.log("buzz")
+    } else {
+        console.log(i)
+    }
+}
